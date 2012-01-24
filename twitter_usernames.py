f4b992c8be243fbdba0eacc752249b6a2b8a66f9
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2012-01-24 17:30:53 -0500

    uncommented debug

--- twitter_usernames.py
+++ twitter_usernames.py
@@ -2,12 +2,13 @@ import sys
 import string
 import requests
 
-letters = ['a','b','c']
 names = []
-# letters = list(string.lowercase)
+letters = list(string.lowercase)
 for c1 in letters:
     for c2 in letters:
         for c3 in letters:
             names.append(c1+c2+c3)
         
 avail = [name for name in names if requests.get("https://api.twitter.com/1/users/show.json?screen_name=" + name).status_code == 404]
+print "Available:"
+avail
